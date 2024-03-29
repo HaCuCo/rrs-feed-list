54924e0d9d5ef67d3c9f10a1c8f29ae8f97ae106
Dnl <rieser.daniel@gmail.com>
2024-03-29 13:29:58 +0100

    init

--- rss-feed-list.js
+++ rss-feed-list.js
@@ -8,3 +8,14 @@ class RssFeedList extends HTMLElement {
         if(!config.entity) throw new Error('Please define an entity');
     }
 }
+
+customElements.define('rss-feed-list', RssFeedList);
+
+window.customCards = window.customCards || [];
+
+window.customCards.push({
+    type: "rss-feed-list",
+    name: "Rss Feed List",
+    preview: true,
+    description: "The List Card generate table with data from sensor that provides data as a list of attributes."
+  });
