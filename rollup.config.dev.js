a6ee72c2b4197a17818929cda5f9769ca5260173
Dnl <rieser.daniel@gmail.com>
2024-03-30 14:58:28 +0100

    Adds ssl and eslint

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,11 +1,12 @@
 import serve from "rollup-plugin-serve";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import fs from "fs";
 
 export default {
   input: ["src/rss-feed-list.js"],
   output: {
-    dir: "./dist",
     format: "es",
+    file: "./dist/rss-feed-list-dev.js",
   },
   plugins: [
     nodeResolve(),
@@ -14,6 +15,10 @@ export default {
       host: "0.0.0.0",
       port: 5000,
       allowCrossOrigin: true,
+      https: {
+        key: fs.readFileSync("key.pem"),
+        cert: fs.readFileSync("cert.pem"),
+      },
       headers: {
         "Access-Control-Allow-Origin": "*",
       },
