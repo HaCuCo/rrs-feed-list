ef7f532b967d7ee774c61d731c403f6d7771b90d
Dnl <rieser.daniel@gmail.com>
2024-03-31 21:19:56 +0200

    Updates config interface to match the new strategy

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,13 +1,17 @@
 import { LovelaceConfig } from 'custom-card-helpers';
 
 export interface HassConfig extends LovelaceConfig {
-  entity: string;
+  entities: HassEntity[];
   rows?: number;
+}
+
+interface HassEntity {
+  entity: string;
+  date_key: string;
   cardTitle?: string;
   entries_key?: string;
   title_key?: string;
   thumbnail_key?: string;
   summary_key?: string;
   link_key?: string;
-  date_key?: string;
 }
